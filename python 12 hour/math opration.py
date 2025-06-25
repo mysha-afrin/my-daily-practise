e9a4b1f4822a0fc16780f50c09bb1b0e48a99d24
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-25 09:34:52 +0600

    Update math opration.py

--- python 12 hour/math opration.py
+++ python 12 hour/math opration.py
@@ -8,3 +8,12 @@ print(test)
 
 user_name = "jeBA mYsha".title().strip()
 print(user_name)
+
+exercise_pet = "I like puppies"
+exercise_pet1 = exercise_pet.replace("puppies", "kitties")
+print(exercise_pet1)
+
+
+exer_pets = "I like puppies, puppies, puppies, puppies"
+exer_pets1 = exer_pets.replace("puppies", "kitten",2)
+print(exer_pets1)
