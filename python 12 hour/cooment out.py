f346747767724ef1250fc7c90a382008d20033ec
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-25 10:32:22 +0600

    Create cooment out.py

---
+++ python 12 hour/cooment out.py
@@ -0,0 +1,29 @@
+exercise_pet = "I like puppies"
+exercise_pet1 = exercise_pet.replace("puppies", "kitties")
+print(exercise_pet1)
+
+
+exer_pets = "I like puppies, puppies, puppies, puppies"
+exer_pets1 = exer_pets.replace("puppies", "kitten",2)
+print(exer_pets1)
+
+
+
+
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+# test_variable = "A really important value"
+####NOTE####
+##IF YOU WANT TO COMMENT OUT MULTIPLE LINES TOGATHER THEN PRESS CTRL AND / TOGATHER##
+####NOTE####
+test_variable = "A really important value"
+test_variable = "A really important value"
+test_variable = "A really important value"
