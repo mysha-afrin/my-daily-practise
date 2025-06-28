7b659dc271cd7c8f605560bd12680e6f557f2790
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-28 10:06:33 +0600

    Create if else flow.py

---
+++ python 12 hour/if else flow.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a number from your mind: "))
+if x > 10:
+   print("Your number is greater than 10.")
+elif x > 20 :
+   print("Your number is greater than 20")
+else:
+   print("You have a bright future ")
