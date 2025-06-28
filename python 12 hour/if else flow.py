b429efcf79388b56a8d067db43f676ac07f9468d
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-28 10:27:42 +0600

    Update if else flow.py

--- python 12 hour/if else flow.py
+++ python 12 hour/if else flow.py
@@ -5,3 +5,14 @@ elif x > 20 :
    print("Your number is greater than 20")
 else:
    print("You have a bright future ")
+
+
+#Work on yourself
+x = int(input("How much money you have ?"))
+print(f"I have {x} taka.")
+if x <= 10:
+   print("Buy a samosa to eat.")
+elif x <= 40:
+   print("Buy a sanwish")
+else:
+   print("You can buy a pizza or burger or some pasta.")
