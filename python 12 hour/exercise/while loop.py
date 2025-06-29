6a5be6ec969ad011ae1946cb854646451eb0beca
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-29 08:07:29 +0600

    Create while loop.py

---
+++ python 12 hour/exercise/while loop.py
@@ -0,0 +1,23 @@
+practise_list = [[10, 40, 20, 50], [2, 42, 10], [101, 10, 4]]
+#Use a for loop to only print numbers below 50.
+
+for nested_list in practise_list:
+    
+    for list in nested_list:
+        
+        while True:
+            if list < 50:
+                print(list)
+                break
+
+
+
+#Skip values below 10.
+
+practise_list = [[10, 40, 20, 50], [2, 42, 10], [101, 10, 4]]
+for nested_list in practise_list:
+    
+    for list in nested_list:
+        
+        if list > 10:
+            print(list)
