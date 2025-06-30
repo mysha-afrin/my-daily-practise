2597de337328fdc659b6613f4f270ab6d95ed80d
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-30 07:34:50 +0600

    Create function.py

---
+++ python 12 hour/exercise/function.py
@@ -0,0 +1,6 @@
+#make a function that have three parametres ,the last one is an operator.
+def better_calculator(num1, num2, operator):
+    if operator == 'plus':
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+better_calculator(2, 4, 'plus' )
