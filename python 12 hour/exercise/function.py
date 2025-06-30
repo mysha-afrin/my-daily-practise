d5b5d79222a2acecd89726a786607f86eb2620af
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-30 07:39:43 +0600

    Update function.py

--- python 12 hour/exercise/function.py
+++ python 12 hour/exercise/function.py
@@ -3,4 +3,8 @@ def better_calculator(num1, num2, operator):
     if operator == 'plus':
         result = num1 + num2
         print(f"{num1} + {num2} = {result}")
+    elif operator == 'minus':
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
 better_calculator(2, 4, 'plus' )
+better_calculator(6, 8, 'minus')
