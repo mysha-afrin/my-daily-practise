83df0ec0d0017e38e9fc0e96a39f46e40c50ee69
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-06-30 20:20:57 +0600

    Update function.py

--- python 12 hour/exercise/function.py
+++ python 12 hour/exercise/function.py
@@ -16,3 +16,11 @@ def test_function(arg1, arg2, arg3, arg4):
     print(arg3)
     print(arg4)
 test_function(1, arg2 = 2, arg3 = 34, arg4 = [2, 3, 4])#Positional argument can not come first.
+
+
+#Create a function with three parametres ; person ,name ,weekdays
+#Person and great should have a default argument'
+def greater_function(person = 'jeba', great ='Hello', weekday = 'Sunday'):
+    print(f"{great} {person} ")
+    print(f"Today is {weekday}")
+greater_function('Jeba', 'greer', 'Monday')
