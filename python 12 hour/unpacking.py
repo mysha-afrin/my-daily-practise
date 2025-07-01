93746ba2880a044c4ec69972a316abdb165086e5
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-01 07:21:40 +0600

    Update unpacking.py

--- python 12 hour/unpacking.py
+++ python 12 hour/unpacking.py
@@ -4,3 +4,11 @@ def print_all(first, *arguments):
     for argument in arguments:
         print(argument)
 print_all(1, 2, 3, 4, 'hello')
+
+
+
+#keyword unpacking.
+def print_more(**arguments):
+    print(arguments)
+    #print all(1, 2, 3, 4, 'hello')
+print_more(arg1 = '1', arg2 = 'test', arg3 = [1, 2, 3])
