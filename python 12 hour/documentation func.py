9c11fbe6d76ba071dd005cdcb61eb9bd81add009
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-03 22:13:37 +0600

    Update documentation func.py

--- python 12 hour/documentation func.py
+++ python 12 hour/documentation func.py
@@ -4,13 +4,13 @@ def test(a, b):
     print(b)
 test(1, 2)
 print(test.__doc__)
-help(test)
+help(test) #
 
 print("Hey there")
 
 
 #If i want return value in function then should use return.
-def test(a:int = 10, b:int = 0) -> int :
+def test(a:int = 10, b:int = 0) -> int : #This action indicates we are expecting our values to be integers.
     print(a)
     print(b)
     return a + b
