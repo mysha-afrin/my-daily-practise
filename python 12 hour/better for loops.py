6d82691b68c62cdf6b50bd01d1848582c22d1777
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-04 06:49:42 +0600

    Update better for loops.py

--- python 12 hour/better for loops.py
+++ python 12 hour/better for loops.py
@@ -1,3 +1,12 @@
 inventiry_names = ['screws', 'wheels', 'Metal parts', 'Rubber bits', 'screw driver', 'wood']
 inventory_num = [23, 45, 78, 49, 50, 36]
 print(list(zip(inventiry_names, inventory_num)))
+
+
+for thing in zip(inventory_num, inventory_names):
+    print(thing)
+    print(thing[0]
+
+
+  for name, number in zip(inventory_names, inventory_num):
+    print(f"{name} current inventory is: {number}")
