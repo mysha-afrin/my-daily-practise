cb56e51dac57d0de007e871f58b0a4346a6aba03
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-05 06:14:22 +0600

    Update list_compre.py

--- python 12 hour/list_compre.py
+++ python 12 hour/list_compre.py
@@ -7,3 +7,11 @@ for num in range(0, 100):
 
 my_list_comprehension = [num for num in range(0, 100)]
 print(my_list_comprehension)
+
+
+
+#Combine a list comprehension.
+my_list = [num for num in range(0, 100) if num< 10] #Here in this condition else don't work.
+print(my_list)
+my_list1 = [num if num < 10 else 0 for num in range(0, 100)] #This line is more powerful than the upper one.
+print(my_list1)
