2629ee26a0da617a9ad85765f2277fed34da75d3
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-05 06:03:12 +0600

    Update list_compre.py

--- python 12 hour/list_compre.py
+++ python 12 hour/list_compre.py
@@ -2,3 +2,8 @@ my_list = []
 for num in range(0, 100):
     my_list.append(num)
     print(my_list)
+
+#The block of code upper and lower works the same.
+
+my_list_comprehension = [num for num in range(0, 100)]
+print(my_list_comprehension)
