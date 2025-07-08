888316125068729278aa8e6381442fbb0d3ef1eb
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-08 06:47:38 +0600

    Update file i o .py

--- python 12 hour/file i o .py
+++ python 12 hour/file i o .py
@@ -2,3 +2,11 @@
 file = open('some.txt')
 print(list(file))
 file.close()
+
+#open and close automatically.
+with open('some.txt') as file:
+    print(type(file.read()))
+
+#open and close automatically.
+with open('some.txt') as file:
+    print(file.read())
