cbda1ed0c5c2a711d828ab99c9b1f58bc8d33b1d
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-10 07:41:59 +0600

    Update class.py

--- python 12 hour/class.py
+++ python 12 hour/class.py
@@ -1,6 +1,11 @@
 class Monster:
+    #attributes
     health = 90
     energy = 40
+    #methods
+    def attack(something, damage):
+        print("the monster has attacked!")
 monster = Monster()
+monster.attack(40)
 print(monster.health)
 print(monster.energy)
