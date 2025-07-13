fee85dee838942b09cbb4e5f84f2cb25232b41b7
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-13 06:19:42 +0600

    Update class.py

--- python 12 hour/class.py
+++ python 12 hour/class.py
@@ -3,10 +3,14 @@ class Monster:
     health = 90
     energy = 40
     #methods
-    def attack(something, damage):
+    def attack(self, damage):
         print("the monster has attacked!")
         print(f"{damage} has dealt.")
+    def move(self, speed):
+        print("The monster has moved!")
+        print(f"It has speed of {speed}.")
 monster = Monster()
 monster.attack(40)
+monster.speed = 40
 print(monster.health)
 print(monster.energy)
