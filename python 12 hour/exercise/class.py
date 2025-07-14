7f9b082b1cb504fceb5f4de22acbe60184370697
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-14 08:19:28 +0600

    Create class.py

---
+++ python 12 hour/exercise/class.py
@@ -0,0 +1,13 @@
+class Monster:
+    def __init__(self, func):
+        self.func = func
+class Attack:
+    def __init__(bite):
+        print(bite)
+    def __init__(strike):
+        print(strike)
+    def __init__(slash):
+        print(slash)
+    def __init__(punch):
+        print(punch)
+attacks = Attack()
