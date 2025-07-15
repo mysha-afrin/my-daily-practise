9ac554f87b749b23cf4149f60af9652909aa199f
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-15 08:12:12 +0600

    Update scope and class.py

--- python 12 hour/exercise/scope and class.py
+++ python 12 hour/exercise/scope and class.py
@@ -1,3 +1,13 @@
+class Monster:
+    def __init__(self, health, energy):
+        self.health = health
+        self.energy = energy
+    def update_energy(self, amount):
+        self.energy += amount
+    def get_damage(self, amount):
+        self.health -= amount
+
+
 #create a hero class with 2 parametres:damage, monster
 class Hero:
     def __init__(self, damage, monster):
@@ -5,9 +15,5 @@ class Hero:
         self.monster = monster
 #the monster class should have a method that lowers the health->get_amount(amount)
 #the hero class should have an attack method
-def get_damage(self, amount):
-        self.damage += amount
-    def get_damage(self, attack):
-        self.monster += attack
-hero = Hero(damage = 100, monster = 50)
-print(hero.damage)
+
+
