4185a92166cf596a05e83d930caab78882670714
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-15 07:51:23 +0600

    Create scope and class.py

---
+++ python 12 hour/exercise/scope and class.py
@@ -0,0 +1,13 @@
+#create a hero class with 2 parametres:damage, monster
+class Hero:
+    def __init__(self, damage, monster):
+        self.damage = damage
+        self.monster = monster
+#the monster class should have a method that lowers the health->get_amount(amount)
+#the hero class should have an attack method
+def get_damage(self, amount):
+        self.damage += amount
+    def get_damage(self, attack):
+        self.monster += attack
+hero = Hero(damage = 100, monster = 50)
+print(hero.damage)
