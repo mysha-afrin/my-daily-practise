232d82e69ff644038c10af172dee9656ee5c6bdc
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-18 06:36:49 +0600

    Update modules_intro.py

--- python 12 hour/modules_intro.py
+++ python 12 hour/modules_intro.py
@@ -1,5 +1,6 @@
 import random ,string
 from math import floor
+from random import * #This code imports all functions from the random module
 
 random_number = random.randint(1, 10)# This code generates a random integer between 1 and 10
 print(random_number)
