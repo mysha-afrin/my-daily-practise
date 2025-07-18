51c8f6bd887e526c881611c74bedc80345dbd1d5
Mysha Afrin Jeba <myshaafrinjeba916@gmail.com>
2025-07-18 06:32:44 +0600

    Update and rename random_modules.py to modules_intro.py

---
+++ python 12 hour/modules_intro.py
@@ -0,0 +1,10 @@
+import random ,string
+from math import floor
+
+random_number = random.randint(1, 10)# This code generates a random integer between 1 and 10
+print(random_number)
+
+test_list = ['hello', True, [1, 2, 3], 6, 8]
+print(random.choice(test_list))  # This code randomly selects an element from the list
+print(string.ascii_letters)  # This code prints all ASCII letters (both lowercase and uppercase)
+print(floor(4.9))
